ccbce9d80772346086b53f8e890495cc2d2e633e
lvalois <leonangabrielvalois@gmail.com>
2024-08-08 17:20:46 -0300

    Tratando peças capturadas.

--- src/application/UI.java
+++ src/application/UI.java
@@ -6,8 +6,11 @@ import chess.ChessPiece;
 import chess.ChessPosition;
 import chess.Color;
 
+import java.util.Arrays;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class UI {
 
@@ -56,11 +59,14 @@ public class UI {
         }
         System.out.println("  a b c d e f g h");
     }
-    public static void printMatch(ChessMatch chessMatch) {
+    public static void printMatch(ChessMatch chessMatch, List<ChessPiece> captured) {
         printBoard(chessMatch.getPieces());
+        System.out.println();
+        printCapturedPieces(captured);
         System.out.println();
         System.out.println("Turno: " + chessMatch.getTurn());
         System.out.println("Aguardando jogador: " + chessMatch.getCurrentPlayer());
+
     }
     public static void printBoard(ChessPiece [][] pieces, boolean [][] possibleMoves) {
         for (int i = 0; i < pieces.length; i++){
@@ -90,6 +96,21 @@ public class UI {
         }
         System.out.print(" ");
     }
+    private static void printCapturedPieces(List<ChessPiece> captured) {
+        List<ChessPiece> white = captured.stream().filter(x -> x.getColor() == Color.white).collect(Collectors.toList());
+        List<ChessPiece> black = captured.stream().filter(x -> x.getColor() == Color.black).collect(Collectors.toList());
+        System.out.println("Captured pieces: ");
+        System.out.print("White: ");
+        System.out.print(ANSI_WHITE);
+        System.out.println(Arrays.toString(white.toArray()));
+        System.out.print(ANSI_RESET);
+
+        System.out.print("Black: ");
+        System.out.print(ANSI_YELLOW);
+        System.out.println(Arrays.toString(black.toArray()));
+        System.out.print(ANSI_RESET);
+    }
+
     public static void clearScreen() {
         System.out.print("\033[H\033[2J");
         System.out.flush();
